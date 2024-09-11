ba74eac454d403d68cd5c62ce6c7c2e4c3308ea5
VictorArt97 <Bakalovvictor@gmail.com>
2024-09-11 20:10:24 +0200

    ejercicio1

--- Assets/Scripts/Ejercicio1_1.cs
+++ Assets/Scripts/Ejercicio1_1.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// Declarar variables 
+    int vida;     // el valor por defecto es un 0 
+    float porcentajeAcierto;   // el valor por defecto es un 0.0f 
+    bool quemado;                 // el valor por defecto es False
+    string mensaje,mensaje2,mensaje3;              // el valor por defecto es ""
+    char solucionPuzzle;
+
     void Start()
     {
-        
+        mensaje = "Hola Mundo ";
+        mensaje2 = "Este es el primer videojuego de victor "; 
+        mensaje3 = "Estoy aprendiendo C#"; 
+        Debug.Log(mensaje + mensaje2+mensaje3);
+      
+
+
+
+
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        
+        Debug.Log("Ha pasado un frame");   //se repite por cada frame 
     }
 }
